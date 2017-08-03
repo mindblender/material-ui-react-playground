9b046da16097f380075d0d5d3752e050f18890ff
Jeff Sittler <mindblender@gmail.com>
2017-08-02 21:18:21 -0700

    code clean-up

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,31 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Button from 'material-ui/Button';
-// import AwesomeComponent from './AwesomeComponent.jsx'
-// import injectTapEventPlugin from 'react-tap-event-plugin';
 
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-// injectTapEventPlugin();
-
-// const App = () => (
-//   <MuiThemeProvider>
-//     <AwesomeComponent />
-//   </MuiThemeProvider>
-// );
 const App = () => (
     <Button raised color="primary" >
         Primary
     </Button>
 );
-
-
-
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('app')
-// );
-
 
 render(<App/>, document.getElementById('app'));
